5832791ab9c502fee32ebfd6db8dc8ee3e500a53
aryafmdev <muhammadaryafm@gmail.com>
2025-12-15 19:53:13 +0700

    chore(space-background): adjust star size and twinkle speed for smoother cosmic animation effect

--- components/SpaceBackground.tsx
+++ components/SpaceBackground.tsx
@@ -11,51 +11,51 @@ export function SpaceBackground() {
         dpr={[1, 2]}
       >
         <Stars
-          radius={130}
-          depth={80}
-          count={10000}
-          factor={6}
+          radius={140}
+          depth={100}
+          count={14000}
+          factor={7}
           saturation={0}
           fade
-          speed={0.85}
+          speed={1.5}
         />
         <Sparkles
-          count={640}
-          size={3.2}
-          speed={2.2}
-          opacity={0.9}
+          count={900}
+          size={23.4}
+          speed={1.6}
+          opacity={0.92}
           color='#91ff02'
           scale={[80, 80, 80]}
         />
         <Sparkles
-          count={300}
-          size={3.8}
-          speed={2.3}
+          count={450}
+          size={4}
+          speed={2.6}
+          opacity={0.65}
+          color='#91ff02'
+          scale={[70, 70, 70]}
+        />
+        <Sparkles
+          count={450}
+          size={2.8}
+          speed={2.2}
           opacity={0.6}
           color='#91ff02'
           scale={[70, 70, 70]}
         />
         <Sparkles
-          count={300}
-          size={2.6}
-          speed={2.0}
-          opacity={0.55}
-          color='#91ff02'
-          scale={[70, 70, 70]}
-        />
-        <Sparkles
-          count={500}
+          count={700}
           size={1.2}
-          speed={3.0}
-          opacity={0.35}
+          speed={3.4}
+          opacity={0.38}
           color='#daffab'
           scale={[90, 90, 90]}
         />
         <Sparkles
-          count={450}
-          size={1}
-          speed={3.2}
-          opacity={0.3}
+          count={650}
+          size={22}
+          speed={3.6}
+          opacity={0.32}
           color='#daffab'
           scale={[110, 110, 110]}
         />
